501279b1bb8a3da39f2e737bad7c8b94ec5ffa7c
ericdumortier1984 <ericdumortier1984@gmail.com>
2024-01-01 09:22:21 -0300

    space reparado

--- Proyecto 6.0/Space/Space.cpp
+++ Proyecto 6.0/Space/Space.cpp
@@ -6,12 +6,13 @@ using namespace sf;
 int main() {
     //creamos ventana y objeto
     RenderWindow _wnd(VideoMode(800, 600), "2- Space");
+    _wnd.setFramerateLimit(60);
     CircleShape ball(50.0f);
     ball.setFillColor(Color::Yellow);
 
-    Vector2f position(0.0f, 100.0f);
-    Vector2f velocity(100.f, 0.0f);
-    const float acceleration = 25.0f;
+    Vector2f position(0.0f, 0.0f);
+    Vector2f velocity(0.0f, 0.0f);
+    const float acceleration = 10.0f;
 
     //loop
     while (_wnd.isOpen()) {
@@ -21,18 +22,18 @@ int main() {
                 _wnd.close();
             }   
         }
-        float deltaTime = 0.1f / 60.0f;
-        velocity.x += acceleration * deltaTime;
-        position += velocity * deltaTime;
+        float deltaTime = 1.0f / 60.0f;
 
         if (evt.type == Event::KeyPressed) {
             if (evt.key.code == Keyboard::Right) {
-                velocity.x++;
+                velocity.x += acceleration;
             }
             if (evt.key.code == Keyboard::Left) {
-                velocity.x--;
+                velocity.x -= acceleration;
             }
         }
+
+        position += velocity * deltaTime;
 
         _wnd.clear();
         ball.setPosition(position);
